9b76183a80fcf643f70a05ac6f30c8178ef88f1d
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2018-09-05 20:40:47 -0500

    c++ version

--- includes/lexer.h
+++ includes/lexer.h
@@ -1,26 +1,28 @@
-//
-// Created by colby on 8/20/18.
-//
-
-#ifndef SEALION_LEXER_H
-#define SEALION_LEXER_H
-
+#include <string>
+#include <map>
+#include <vector>
+#include "tokens.h"
 #include "token.h"
+namespace SeaMonkey {
+	class Lexer
+	{
+	private:
+		std::string input;
+		unsigned int position;
+		unsigned int readPosition;
+		char ch;
+		std::map<std::string, TokenType> keywords;
 
-typedef struct Lexer {
-    char* input;
-    int position;
-    int readPosition;
-    char ch;
-} Lexer;
-
-Lexer* NewLexer(char* input);
-void readChar(Lexer* lexer);
-Token newToken(TokenType tokenType, char ch);
-Token nextToken(Lexer* lexer);
-char* readIdentifier(Lexer* lexer);
-void skipWhitespace(Lexer* lexer);
-char* readNumber(Lexer* lexer);
-char peekChar(Lexer* lexer);
-
-#endif //SEALION_LEXER_H
+		void readChar();
+		Token* nextToken();
+		std::string readIdentifier();
+		std::string readNumber();
+		bool isLetter(char ch);
+		void skipWhitespace();
+		char peekChar();
+		std::string lookupIdentity(std::string identity);
+	public:
+		Lexer(std::string input);
+		std::vector<Token*>* Tokenize();
+	};
+}
--- includes/token.h
+++ includes/token.h
@@ -1,49 +1,12 @@
-//
-// Created by colby on 8/20/18.
-//
+#include <string>
+namespace SeaMonkey
+{
+	typedef std::string TokenType;
 
-#ifndef SEALION_TOKEN_H
-#define SEALION_TOKEN_H
+	struct Token {
+		TokenType type;
+		std::string literal;
 
-typedef char* TokenType;
-
-typedef struct Token {
-    TokenType type;
-    char* literal;
-} Token;
-
-TokenType LookupIdentity(char* ident);
-
-extern char* ILLEGAL;
-extern char* EoF;
-
-extern char* IDENT;
-extern char* INT;
-
-extern char* ASSIGN;
-extern char* PLUS;
-extern char* MINUS;
-extern char* BANG;
-extern char* ASTERISK;
-extern char* SLASH;
-extern char* LT;
-extern char* GT;
-
-extern char* COMMA;
-extern char* SEMICOLON;
-extern char* LPAREN;
-extern char* RPAREN;
-extern char* LBRACE;
-extern char* RBRACE;
-
-extern char* FUNCTION;
-extern char* LET;
-extern char* TRUE;
-extern char* FALSE;
-extern char* IF;
-extern char* ELSE;
-extern char* RETURN;
-
-extern char* EQ;
-extern char* NOT_EQ;
-#endif //SEALION_TOKEN_H
+		Token(TokenType type, std::string literal);
+	};
+}
